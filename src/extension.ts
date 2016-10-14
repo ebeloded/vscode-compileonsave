24461c4151056a06af85388a384e0464ba45abb8
Eugene Beloded <belodedevgenij@gmail.com>
2016-10-14 23:50:29 +0700

    Allow comments in tsconfig.json

--- src/extension.ts
+++ src/extension.ts
@@ -6,46 +6,56 @@ import * as fs from 'fs'
 import { spawn, exec, execFile } from 'child_process'
 
 const kill = require('tree-kill');
-
+const stripJSONComments = require('strip-json-comments')
 const tscProcesses = {};
 let watcher, outputChannel;
 
+console.log('loading extension');
+
 const configureOutputChannel = () => {
+    console.log('create output channel')
     outputChannel = vscode.window.createOutputChannel('TypeScript Compiler')
 }
 
 const disposeOutputChannel = () => {
+    console.log('dispose output channel');
     if (outputChannel)
         outputChannel.dispose();
 }
 let cleanupChannelOnNextRound = false;
 
 const updateOutputChannel = (data) => {
+    console.log('OUTPUT', data);
     if (cleanupChannelOnNextRound) {
         outputChannel.clear();
+        outputChannel.hide();
         cleanupChannelOnNextRound = false;
     }
     var dataString = `${data}`;
 
     if (dataString.indexOf('error') > -1) {
         outputChannel.append(dataString);
+        outputChannel.show(true);
     } else {
         cleanupChannelOnNextRound = true;
     }
 }
 
 const launchTSC = (tsconfigPath) => {
-
+    console.log('launch tsc', tsconfigPath);
     const tsc = spawn('tsc', ['-p', `"${tsconfigPath}"`, '-w'], { shell: true });
 
     tsc.stdout.on('data', (data) => updateOutputChannel(`${data}`));
 
     tsc.on('close', (code, signal) => delete tscProcesses[tsc.pid]);
 
+    tsc.on('error', (err) => console.error('tsc error:', err))
+
     tscProcesses[tsconfigPath] = tsc.pid;
 }
 
 const stopTSC = (tsconfigPath) => {
+    console.log('stop tsc', tsconfigPath);
     if (tscProcesses[tsconfigPath]) {
         kill(tscProcesses[tsconfigPath]);
     }
@@ -54,15 +64,20 @@ const stopTSC = (tsconfigPath) => {
 const stopAllTSC = () => Object.keys(tscProcesses).forEach(key => stopTSC(key))
 
 const processTsConfig = tsconfigPath => {
+    console.log('process tsconfig', tsconfigPath);
     stopTSC(tsconfigPath);
     try {
         const data = fs.readFileSync(tsconfigPath, 'utf8');
-        const tsconfig = JSON.parse(data);
+        console.log('data', data);
+        const tsconfig = JSON.parse(stripJSONComments(data));
+        console.log('tsconfig:', tsconfig);
         if (tsconfig.compileOnSave === true) {
             launchTSC(tsconfigPath)
             return;
         }
-    } catch (e) { }
+    } catch (e) {
+        console.log('catch', e);
+    }
 
 }
 
@@ -85,6 +100,7 @@ const disposeWatcher = () => {
 
 export function activate(context: vscode.ExtensionContext) {
 
+    console.log('activate compileOnSave extension')
     if (vscode.workspace.rootPath) {
         configureOutputChannel();
         vscode.workspace.findFiles('**/tsconfig.json', '**/node_modules/**').then((files) => {
@@ -101,6 +117,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
+    console.log('dispose');
     stopAllTSC();
     disposeWatcher();
     disposeOutputChannel();
